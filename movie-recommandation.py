d88e3bf0abfc8934e70ef473fbc89053d3334d61
Penciler <eraser41@livemail.tw>
2018-04-16 21:05:46 +0800

    add note to movie-recommendation_01

--- movie-recommandation.py
+++ movie-recommandation.py
@@ -2,17 +2,30 @@ import numpy as np
 from lightfm.datasets import fetch_movielens
 from lightfm import LightFM
 
+#get 49906+5469 data
 data=fetch_movielens(min_rating=4.0)
 
+#show how many training and test data in the dataset
 print(repr(data['train']))
 print(repr(data['test']))
 
+#just want to see how "shape" works and how dataset looks like
+print(data['train'].shape)
+print(data['test'])
+
+#define model parameters
 model=LightFM(loss='warp')
 
+#feed training data to model
 model.fit(data['train'], epochs=30, num_threads=2)
 
+#define function for recommandation
 def sample_recommendation(model, data, user_ids):
+#use "n_users" , "n_items" to record the dimension of dataset
   n_users, n_items=data['train'].shape
+  print(n_users)
+  print(n_items)
+#iterate through "user_ids" array
   for user_id in user_ids:
     known_positives=data['item_labels'][data['train'].tocsr()[user_id].indices]
     scores=model.predict(user_id, np.arange(n_items))
